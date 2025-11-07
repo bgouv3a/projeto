d9a9c7937b35cfa1ba67abb5e20c01b6446fed39
bgouv3a <brayan.gouvearocha@gmail.com>
2025-11-07 11:54:30 -0300

    principal
    aaa

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log(ola mundo)
+console.log("ola mundo");
+
+let churrasco = "sim";
+
+if ( churrasco =="sim") {
+    alert ("vai ter churrasco!");
+} else[
+    alert("Entao vamos marcar o churrascoswssssssssss")
+]
